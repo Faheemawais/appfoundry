a91356224056ab20d62480b1938384f35416af69
hamza hadi <faheemawais481@gmail.com>
2026-07-03 02:30:46 +0500

    Added new projects

--- data/projects.ts
+++ data/projects.ts
@@ -40,6 +40,45 @@ export const projects: Project[] = [
     price: "$800–$1000",
     platform: "iOS & Android",
   },
+  {
+    id: 3,
+    title: "Personal Finance & Digital Banking App",
+    subtitle: "Smart banking, better living",
+    description:
+      "A simple and secure way to manage your finances, track expenses, monitor account balances, and analyze spending habits.",
+    tags: ["FlutterFlow", "Firebase", "Flutter"],
+    image: "/projects/finaceApp.png",
+    date: "March 2026",
+    duration: "1–3 Months",
+    price: "$800–$1000",
+    platform: "iOS & Android",
+  },
+  {
+    id: 4,
+    title: "Money Watch",
+    subtitle: "Watch & Earn Mobile App",
+    description:
+      "The app allows users to watch videos, earn rewards, and withdraw their earnings securely through multiple payment methods such as PayPal and bank transfer.",
+    tags: ["Firebase", "Flutter"],
+    image: "/projects/earnmoney.png",
+    date: "March 2026",
+    duration: "7-30 Days",
+    price: "$600–$800",
+    platform: "iOS & Android",
+  },
+  {
+    id: 5,
+    title: "LITTLE JOY CLUB",
+    subtitle: "Self-Love & Mental Wellness Mobile App",
+    description:
+      "The app helps users build healthy habits through guided affirmations, journaling, mindfulness exercises, mood tracking, and daily self-love activities.",
+    tags: ["Firebase", "Flutter", "Dart"],
+    image: "/projects/littlejoyclub.png",
+    date: "March 2026",
+    duration: "7-30 Days",
+    price: "$600–$800",
+    platform: "iOS & Android",
+  },
 ];
 
 // ✅ To add a new project in future:
